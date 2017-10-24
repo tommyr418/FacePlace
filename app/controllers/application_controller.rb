499154598bc9552d64da8df761fb45b9da8f154e
Tommy Ren <tommyr418@gmail.com>
2017-10-24 12:32:59 -0400

    add session controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :logged_in?, :current_user
+
+  def logged_in?
+  end
+
+  def current_user
+  end
+
+  def login(user)
+  end
+
+  def logout
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "static_pages#root"
+  root to: "static_page#root"
 
   namespace :api, default: { format: :json } do
     resources :users, only: [:show, :create, :edit]
