bb190139e9d507cd63e01d456bc9dbcfa64aaa83
Tommy Ren <tommyr418@gmail.com>
2017-10-24 13:39:12 -0400

    add helper methods and complete session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,24 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
 
+    if @user
+      login(@user)
+      render :show
+    else
+      render json: ["Invalid Email or Password"], status: 401
+    end
   end
 
   def destroy
-
+    if current_user
+      logout
+      render json: {}
+    else
+      render json: ["Already logged out"], status: 401
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,24 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :logged_in?, :current_user
 
+  private
+
   def logged_in?
+    !!current_user
   end
 
   def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
   end
 
   def login(user)
+    @current_user = user
+    session
   end
 
   def logout
+    @current_user.reset_session_token!
+    @current_user = nil
+    session[:session_token] = nil
   end
-  
 end
