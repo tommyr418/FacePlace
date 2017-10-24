659536cd329ae05ed3b2d60938bc659876a7aae0
Tommy Ren <tommyr418@gmail.com>
2017-10-24 12:05:26 -0400

    add static page controller and view

---
+++ app/controllers/static_page_controller.rb
@@ -0,0 +1,4 @@
+class StaticPageController < ApplicationController
+  def root
+  end
+end
---
+++ test/controllers/static_page_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class StaticPageControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
