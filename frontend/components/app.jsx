5a7ec7d5c4fa60341b1ceda818ba6d5942ec31de
Tommy Ren <tommyr418@gmail.com>
2017-10-26 11:27:26 -0400

    add route util for protected routes

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import MainHeaderContainer from './header/main_header_container';
 import MainContainer from './main/main_container';
+import { Route } from 'react-router-dom';
+import ProtectedRoute from '../util/route_util';
 
 const App = () => (
   <div>
---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { connect }  from 'react-redux';
+import { Route, Redirect, withRouter } from 'react-router-dom';
+
+const Protected = ({ component: Component, path, loggedIn }) => (
+  <Route path={path} render={(props) => (
+    loggedIn ? (
+      <Component {...props} />
+    ) : (
+      <Redirect to="/" />
+    )
+  )} />
+);
+
+const mapStateToProps = state => (
+  {loggedIn: Boolean(state.session.currentUser)}
+);
+
+export const ProtectedRoute = withRouter(connect(
+  mapStateToProps,
+  null
+)(Protected));
