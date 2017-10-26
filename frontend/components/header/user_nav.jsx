c03f1983c8989ba0cd0f28e32a66b30ff71af8a2
Tommy Ren <tommyr418@gmail.com>
2017-10-26 11:12:43 -0400

    add link to profile page

--- frontend/components/header/user_nav.jsx
+++ frontend/components/header/user_nav.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const UserNav = ({ logout, currentUser }) => {
   return (
     <nav id="user-nav">
-      <span>{ currentUser.fname }</span>
+      <Link to={`users/${currentUser.id}`}>
+      { currentUser.fname }
+      </Link>
       <button onClick={ logout }>Logout</button>
     </nav>
   );
