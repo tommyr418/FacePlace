816bb5fe188236094918d0906e6fb99c0812d16a
Tommy Ren <tommyr418@gmail.com>
2017-10-30 10:06:52 -0400

    add left pane on login screen

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -20,6 +20,27 @@ class Main extends React.Component {
     } else {
       return (
         <div id="main">
+          <div id="main-front-left">
+            <h1>Connect with friends and the world around you on FacePlace.</h1>
+
+            <div>
+              <i className="fa fa-newspaper-o" aria-hidden="true"></i>
+              <h2>See photos and updates</h2>
+              <h3>from friends in News Feed.</h3>
+            </div>
+
+            <div>
+              <i className="fa fa-desktop" aria-hidden="true"></i>
+              <h2>Share what's new</h2>
+              <h3>in your life on your Timeline.</h3>
+            </div>
+
+            <div>
+              <i className="fa fa-share-alt" aria-hidden="true"></i>
+              <h2>Find more</h2>
+              <h3>of what you're looking for with Facebook Search.</h3>
+            </div>
+          </div>
 
           <SignupForm signup={ this.props.signup }/>
         </div>
