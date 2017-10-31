525697c959dca61a3d5c185cf18d824ba4a1d672
Tommy Ren <tommyr418@gmail.com>
2017-10-30 23:37:27 -0400

    add form for uploading pictures

--- frontend/components/main-show/profile_picture_upload.jsx
+++ frontend/components/main-show/profile_picture_upload.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
-const ProfilePictureUpload = () => {
-  return (
-    <div id="profile-picture-upload">
+class ProfilePictureUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageUrl: "",
+      imageFile: null,
+    };
+    this.handleFile = this.handleFile.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    </div>
-  );
-};
+  handleFile(e) {
+    const reader = new FileReader();
+    const file = e.currentTarget.files[0];
+    reader.onloadend = () =>
+      this.setState({ imageUrl: reader.result, imageFile: file});
+
+    if (file) {
+      reader.readAsDataURL(file);
+    } else {
+      this.setState({ imageUrl: "", imageFile: null });
+    }
+  }
+
+  handleSubmit() {
+    
+  }
+
+  render() {
+    return (
+      <form id="profile-picture-upload">
+        <input type="file"
+          onChange={ this.handleFile }
+          onSubmit={ this.handleSubmit }>
+        </input>
+        <img src={ this.state.imageUrl }/>
+        <button>Submit</button>
+      </form>
+    );
+  }
+}
 
 export default ProfilePictureUpload;
