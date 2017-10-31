5870b29d1435bc5eb08b68a3a32920e3dffcb4d6
Tommy Ren <tommyr418@gmail.com>
2017-10-30 23:48:40 -0400

    fix rendering of cover photo

--- frontend/components/main-show/cover_photo.jsx
+++ frontend/components/main-show/cover_photo.jsx
@@ -3,11 +3,19 @@ import React from 'react';
 import ProfilePicture from './profile_picture';
 
 const CoverPhoto = ({ user, currentUser}) => {
-  if (currentUser.id === user.id || currentUser.friends.includes(user.id)) {
+  if (currentUser.id === user.id) {
     return (
       <div id="cover-photo">
-        <button>Add Cover Photo</button>
+        <button>Add/Update Cover Photo</button>
 
+        <ProfilePicture user={ user }
+          currentUser={ currentUser }/>
+        <img />
+      </div>
+    );
+  } else if (currentUser.friends.includes(user.id)) {
+    return (
+      <div id="cover-photo">
         <ProfilePicture user={ user }
           currentUser={ currentUser }/>
         <img />
@@ -16,7 +24,6 @@ const CoverPhoto = ({ user, currentUser}) => {
   } else {
     return (
       <div id="cover-photo">
-        <button>Add Cover Photo</button>
 
         <ProfilePicture user={ user }
           currentUser={ currentUser }/>
