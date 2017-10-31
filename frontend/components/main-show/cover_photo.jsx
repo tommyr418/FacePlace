e692eac3b10d231eee884d21e2c82b8bc7d0edc4
Tommy Ren <tommyr418@gmail.com>
2017-10-31 11:37:21 -0400

    refactor cover photo component

--- frontend/components/main-show/cover_photo.jsx
+++ frontend/components/main-show/cover_photo.jsx
@@ -2,39 +2,38 @@ import React from 'react';
 
 import ProfilePicture from './profile_picture';
 
-const CoverPhoto = ({ user, currentUser, updateProfilePic }) => {
-  if (currentUser.id === user.id) {
-    return (
-      <div id="cover-photo">
-        <button>Add/Update Cover Photo</button>
+class CoverPhoto extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <ProfilePicture user={ user }
-          currentUser={ currentUser }
-          updateProfilePic={ updateProfilePic }/>
-        <img src={ user.cover_image_url }/>
-      </div>
-    );
-  } else if (currentUser.friends.includes(user.id)) {
-    return (
-      <div id="cover-photo">
-        <ProfilePicture user={ user }
-          currentUser={ currentUser }
-          updateProfilePic={ updateProfilePic }/>
-        <img src={ user.cover_image_url }/>
-      </div>
-    );
-  } else {
-    return (
-      <div id="cover-photo">
+  }
 
-        <ProfilePicture user={ user }
-          currentUser={ currentUser }
-          updateProfilePic={ updateProfilePic }/>
-        <img src={ user.cover_image_url }/>
-        <button>Add Friend</button>
+
+  render() {
+    const addCover = (this.props.currentUser.id === this.props.user.id) ?
+      <button id="add-cover-button">Add/Update Cover Photo</button>
+      :
+      "";
+    const addFriend = ((this.props.currentUser.id === this.props.user.id) || 
+      this.props.currentUser.friends.includes(
+      this.props.user.id)) ?
+      ""
+      :
+      <button id="add-friend-button">Add Friend</button>;
+
+    return (
+      <div id="cover-photo">
+        { addCover }
+
+        <ProfilePicture user={ this.props.user }
+          currentUser={ this.props.currentUser }
+          updateProfilePic={ this.props.updateProfilePic }/>
+        <img src={ this.props.user.cover_image_url }/>
+
+        { addFriend }
       </div>
     );
   }
-};
+}
 
 export default CoverPhoto;
