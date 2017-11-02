3d696964c1f3168edce573fcb713d8202b12cbaf
Tommy Ren <tommyr418@gmail.com>
2017-11-01 20:27:48 -0400

    refactors deatils pane

--- frontend/components/main-show/about/about_details_about_you.jsx
+++ frontend/components/main-show/about/about_details_about_you.jsx
@@ -1,95 +1,97 @@
 import React from 'react';
 
-const AboutDetailsAboutYou = ({ profile, userId }) => {
-  if(profile["detailsAboutYou"]) {
-    return (
-      <div className="about-details">
-        <label>About you</label>
-        {
-          profile["detailsAboutYou"]["aboutYou"] ?
-          <ul>
-            { profile["detailsAboutYou"]["aboutYou"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
-          </ul>
-          :
-          null
-        }
+class AboutDetailsAboutYou extends React.Component {
+  render() {
+    if(this.props.profile["detailsAboutYou"]) {
+      return (
+        <div className="about-details">
+          <label>About you</label>
+          {
+            this.props.profile["detailsAboutYou"]["aboutYou"] ?
+            <ul>
+              { this.props.profile["detailsAboutYou"]["aboutYou"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
+            </ul>
+            :
+            null
+          }
 
-        <label>Name Pronunciation</label>
-        {
-          profile["detailsAboutYou"]["namePronunciation"] ?
-          <ul>
-            { profile["detailsAboutYou"]["namePronunciation"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
-          </ul>
-          :
-          null
-        }
+          <label>Name Pronunciation</label>
+          {
+            this.props.profile["detailsAboutYou"]["namePronunciation"] ?
+            <ul>
+              { this.props.profile["detailsAboutYou"]["namePronunciation"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
+            </ul>
+            :
+            null
+          }
 
-        <label>Other Names</label>
-        {
-          profile["detailsAboutYou"]["otherNames"] ?
-          <ul>
-            { profile["detailsAboutYou"]["otherNames"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
-          </ul>
-          :
-          null
-        }
+          <label>Other Names</label>
+          {
+            this.props.profile["detailsAboutYou"]["otherNames"] ?
+            <ul>
+              { this.props.profile["detailsAboutYou"]["otherNames"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
+            </ul>
+            :
+            null
+          }
 
-        <label>Favorite Quotes</label>
-        {
-          profile["detailsAboutYou"]["favoriteQuotes"] ?
-          <ul>
-            { profile["detailsAboutYou"]["favoriteQuotes"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
-          </ul>
-          :
-          null
-        }
-      </div>
-    );
-  } else{
-    return (
-      <div className="about-details">
-        <label>About You</label>
+          <label>Favorite Quotes</label>
+          {
+            this.props.profile["detailsAboutYou"]["favoriteQuotes"] ?
+            <ul>
+              { this.props.profile["detailsAboutYou"]["favoriteQuotes"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
+            </ul>
+            :
+            null
+          }
+        </div>
+      );
+    } else{
+      return (
+        <div className="about-details">
+          <label>About You</label>
 
-        <label>Name Pronunciation</label>
+          <label>Name Pronunciation</label>
 
-        <label>Other Names</label>
+          <label>Other Names</label>
 
-        <label>Favorite Quotes</label>
-      </div>
-    );
+          <label>Favorite Quotes</label>
+        </div>
+      );
+    }
   }
-};
+}
 
 export default AboutDetailsAboutYou;
