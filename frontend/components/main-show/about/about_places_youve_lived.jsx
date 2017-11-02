bf71e01c1ba9820a019785cfc6472a4e22699338
Tommy Ren <tommyr418@gmail.com>
2017-11-01 20:22:06 -0400

    refactor places in about pane

--- frontend/components/main-show/about/about_places_youve_lived.jsx
+++ frontend/components/main-show/about/about_places_youve_lived.jsx
@@ -1,55 +1,57 @@
 import React from 'react';
 
-const AboutPlacesYouveLived = ({ profile, userId }) => {
-  if(profile["placesYouveLived"]) {
-    return (
-      <div className="about-details">
-        <label>Current City and Hometown</label>
-        {
-          profile["placesYouveLived"]["currentCityAndHometown"] ?
-          <ul>
-            { profile["placesYouveLived"]["currentCityAndHometown"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
-          </ul>
-          :
-          null
-        }
+class AboutPlacesYouveLived extends React.Component {
+  render() {
+    if(this.props.profile["placesYouveLived"]) {
+      return (
+        <div className="about-details">
+          <label>Current City and Hometown</label>
+          {
+            this.props.profile["placesYouveLived"]["currentCityAndHometown"] ?
+            <ul>
+              { this.props.profile["placesYouveLived"]["currentCityAndHometown"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
+            </ul>
+            :
+            null
+          }
 
-        <label>Other Places Lived</label>
-        {
-          profile["placesYouveLived"]["otherPlacesLived"] ?
-          <ul>
-            { profile["placesYouveLived"]["otherPlacesLived"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
-          </ul>
-          :
-          null
-        }
-      </div>
-    );
-  } else{
-    return (
-      <div className="about-details">
-        <label>Current City and Hometown</label>
+          <label>Other Places Lived</label>
+          {
+            this.props.profile["placesYouveLived"]["otherPlacesLived"] ?
+            <ul>
+              { this.props.profile["placesYouveLived"]["otherPlacesLived"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
+            </ul>
+            :
+            null
+          }
+        </div>
+      );
+    } else{
+      return (
+        <div className="about-details">
+          <label>Current City and Hometown</label>
 
-        <label>Other Places Lived</label>
-      </div>
-    );
+          <label>Other Places Lived</label>
+        </div>
+      );
+    }
   }
-};
+}
 
 export default AboutPlacesYouveLived;
