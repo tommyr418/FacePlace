094112b6704d548287ccce318f3dee0ab72b2967
Tommy Ren <tommyr418@gmail.com>
2017-11-01 20:24:13 -0400

    refactor contacts pane

--- frontend/components/main-show/about/about_contact.jsx
+++ frontend/components/main-show/about/about_contact.jsx
@@ -1,75 +1,77 @@
 import React from 'react';
 
-const AboutContact = ({ profile, userId }) => {
-  if(profile["contactAndBasicInfo"]) {
-    return (
-      <div className="about-details">
-        <label>Contact Information</label>
-        {
-          profile["contactAndBasicInfo"]["contactInformation"] ?
-          <ul>
-            { profile["contactAndBasicInfo"]["contactInformation"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
-          </ul>
-          :
-          null
-        }
+class AboutContact extends React.Component {
+  render() {
+    if(this.props.profile["contactAndBasicInfo"]) {
+      return (
+        <div className="about-details">
+          <label>Contact Information</label>
+          {
+            this.props.profile["contactAndBasicInfo"]["contactInformation"] ?
+            <ul>
+              { this.props.profile["contactAndBasicInfo"]["contactInformation"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
+            </ul>
+            :
+            null
+          }
 
-        <label>Websites and Social links</label>
-        {
-          profile["contactAndBasicInfo"]["websitesAndSocialLinks"] ?
-          <ul>
-            { profile["contactAndBasicInfo"]["websitesAndSocialLinks"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
-          </ul>
-          :
-          null
-        }
+          <label>Websites and Social links</label>
+          {
+            this.props.profile["contactAndBasicInfo"]["websitesAndSocialLinks"] ?
+            <ul>
+              { this.props.profile["contactAndBasicInfo"]["websitesAndSocialLinks"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
+            </ul>
+            :
+            null
+          }
 
-        <label>Basic Information</label>
-        {
-          profile["contactAndBasicInfo"]["basicIformation"] ?
-          <ul>
-            { profile["contactAndBasicInfo"]["basicIformation"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
-          </ul>
-          :
-          null
-        }
-      </div>
-    );
-  } else{
-    return (
-      <div className="about-details">
-        <label>Contact Information</label>
+          <label>Basic Information</label>
+          {
+            this.props.profile["contactAndBasicInfo"]["basicIformation"] ?
+            <ul>
+              { this.props.profile["contactAndBasicInfo"]["basicIformation"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
+            </ul>
+            :
+            null
+          }
+        </div>
+      );
+    } else{
+      return (
+        <div className="about-details">
+          <label>Contact Information</label>
 
-        <label>Websites and Social links</label>
+          <label>Websites and Social links</label>
 
-        <label>Basic Information</label>
-      </div>
-    );
+          <label>Basic Information</label>
+        </div>
+      );
+    }
   }
-};
+}
 
 export default AboutContact;
