4edd9886544f3a908b87dd41da1e26d99a5cda0d
Tommy Ren <tommyr418@gmail.com>
2017-11-01 20:26:11 -0400

    refactors family pane

--- frontend/components/main-show/about/about_family_and_relationships.jsx
+++ frontend/components/main-show/about/about_family_and_relationships.jsx
@@ -1,54 +1,56 @@
 import React from 'react';
 
-const AboutFamilyAndRelationships = ({ profile, userId }) => {
-  if(profile["familyAndRelationships"]) {
-    return (
-      <div className="about-details">
-        <label>Relationship</label>
-        {
-          profile["familyAndRelationships"]["relationship"] ?
-          <ul>
-            { profile["familyAndRelationships"]["relationship"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
-          </ul>
-          : null
-        }
-
-        <label>Family Members</label>
-        {
-          profile["familyAndRelationships"]["familyMembers"] ?
-          <ul>
-            { profile["familyAndRelationships"]["familyMembers"].map(
-              (item) =>
-              <li key={ item.id }>
-              { item["value"] }
-              <div>
-                <button>edit</button>
-                <button>delete</button>
-              </div>
-            </li>) }
+class AboutFamilyAndRelationships extends React.Component {
+  render() {
+    if(this.props.profile["familyAndRelationships"]) {
+      return (
+        <div className="about-details">
+          <label>Relationship</label>
+          {
+            this.props.profile["familyAndRelationships"]["relationship"] ?
+            <ul>
+              { this.props.profile["familyAndRelationships"]["relationship"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
             </ul>
-            :
-            null
+            : null
           }
-      </div>
-    );
-  } else{
-    return (
-      <div className="about-details">
-        <label>Relationship</label>
 
-        <label>Family Members</label>
-      </div>
-    );
+          <label>Family Members</label>
+          {
+            this.props.profile["familyAndRelationships"]["familyMembers"] ?
+            <ul>
+              { this.props.profile["familyAndRelationships"]["familyMembers"].map(
+                (item) =>
+                <li key={ item.id }>
+                { item["value"] }
+                <div>
+                  <button>edit</button>
+                  <button>delete</button>
+                </div>
+              </li>) }
+              </ul>
+              :
+              null
+            }
+        </div>
+      );
+    } else{
+      return (
+        <div className="about-details">
+          <label>Relationship</label>
+
+          <label>Family Members</label>
+        </div>
+      );
+    }
   }
-};
+}
 
 export default AboutFamilyAndRelationships;
