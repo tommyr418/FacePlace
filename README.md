ae1a405f2bb449182576aed82497c511ea91c826
Tommy Ren <tommyr418@gmail.com>
2017-11-03 11:47:16 -0400

    add readme

--- README.md
+++ README.md
@@ -1,24 +1,37 @@
-# README
+# FacePlace!
 
-This README would normally document whatever steps are necessary to get the
-application up and running.
+[FacePlace Live](https://faceplace2017.herokuapp.com/)
 
-Things you may want to cover:
+FacePlace is a full-stack social media platform designed to mimic the popular website Facebook. It uses Ruby on Rails to handle the backend, on top of a  PostgresSQL database, with the frontend handled by React.js which uses the Redux architectural framework.
 
-* Ruby version
+## Features
 
-* System dependencies
+### User Profiles
 
-* Configuration
+Profiles are stored modularly in the database as profile entries of which a user can have an unlimited amount. They are associated with a user via a user_id foreign key and presorted into categories and subcategories. In order to create a profile entry, a user simply hits the 'add info' button on their main page and a modal with a modular form will appear. Users can then select a category, subcategory and input a entry
 
-* Database creation
+### Friending
 
-* Database initialization
+Users can add one another to their friends lists by hitting the Add friend button on the potential friend's page, a request will be sent to the potential friend. The potential friend will have pending requests in dropdown menu on their header where they can accept or decline the request.
 
-* How to run the test suite
+### Posting
 
-* Services (job queues, cache servers, search engines, etc.)
+At the user's convenience there is a post creation form on every user page to add a post the that user's wall. users can also post to their own walls from the main page.
 
-* Deployment instructions
+### News Feed
 
-* ...
+All the posts that are related to the user and the user's friends are displayed on the front page.
+
+## Future Directions for FacePlace
+
+### Search
+
+Users should be able to find anyone on the platform by inputing a name on the search bar.
+
+### Events
+
+Users should be able to plan events with the ability to send invitations to friends
+
+### Notifications
+
+Users should be alerted to any changes worthy of news such as a friend posting on their wall, a upcoming birthday or event, etc.
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,51 +7,105 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-user = User.create(email: "homer@yahoo.com", password: "homersimpson",
+user = User.new(email: "homer@yahoo.com", password: "homersimpson",
                    fname: "Homer", lname: "Simpson", birthdate: "04/17/1970",
                    sex: "male")
-f1 = User.create(email: "marge@yahoo.com", password: "margesimpson",
+
+file = File.open('app/assets/images/homer.jpg')
+user.profile_pic = file
+user.save!
+
+f1 = User.new(email: "marge@yahoo.com", password: "margesimpson",
                  fname: "Marge", lname: "Simpson", birthdate: "10/10/1973",
                  sex: "female")
-f2 = User.create(email: "bart@yahoo.com", password: "bartsimpson", fname: "Bart",
+
+file = File.open('app/assets/images/marge.png')
+f1.profile_pic = file
+f1.save!
+
+f2 = User.new(email: "bart@yahoo.com", password: "bartsimpson", fname: "Bart",
             lname: "Simpson", birthdate: "12/30/1992", sex: "male")
-f3 =  User.create(email: "lisa@yahoo.com", password: "lisasimpson", fname: "Lisa",
+
+file = File.open('app/assets/images/bart.png')
+f2.profile_pic = file
+f2.save!
+
+f3 =  User.new(email: "lisa@yahoo.com", password: "lisasimpson", fname: "Lisa",
             lname: "Simpson", birthdate: "01/18/1994", sex: "female")
-u1 = User.create(email: "ned@yahoo.com", password: "nedflanders", fname: "Ned",
+
+file = File.open('app/assets/images/lisa.png')
+f3.profile_pic = file
+f3.save!
+
+u1 = User.new(email: "ned@yahoo.com", password: "nedflanders", fname: "Ned",
             lname: "Flanders", birthdate: "12/25/1945", sex: "male")
-u2 = User.create(email: "maude@yahoo.com", password: "maudeflanders", fname: "Maude",
+
+file = File.open('app/assets/images/ned.png')
+u1.profile_pic = file
+u1.save!
+
+u2 = User.new(email: "maude@yahoo.com", password: "maudeflanders", fname: "Maude",
             lname: "Flanders", birthdate: "12/02/1952", sex: "female")
-f4 = User.create(email: "moe@yahoo.com", password: "moesizlack", fname: "Moe",
+
+file = File.open('app/assets/images/maude.png')
+u2.profile_pic = file
+u2.save!
+
+f4 = User.new(email: "moe@yahoo.com", password: "moesizlack", fname: "Moe",
             lname: "Sizlack", birthdate: "07/12/1959", sex: "male")
-f5 = User.create(email: "barney@yahoo.com", password: "barneygumble",
+
+file = File.open('app/assets/images/moe.jpg')
+f4.profile_pic = file
+f4.save!
+
+f5 = User.new(email: "barney@yahoo.com", password: "barneygumble",
             fname: "Barney",
             lname: "Gumble", birthdate: "05/14/1965", sex: "male")
-f6 = User.create(email: "lenny@yahoo.com", password: "lennyleonard", fname: "Lenny",
+
+file = File.open('app/assets/images/barney.png')
+f5.profile_pic = file
+f5.save!
+
+f6 = User.new(email: "lenny@yahoo.com", password: "lennyleonard", fname: "Lenny",
             lname: "Leonard", birthdate: "11/27/1970", sex: "male")
-f7 = User.create(email: "carl@yahoo.com", password: "carlcarlsom", fname: "Carl",
+
+file = File.open('app/assets/images/lenny.jpg')
+f6.profile_pic = file
+f6.save!
+
+f7 = User.new(email: "carl@yahoo.com", password: "carlcarlsom", fname: "Carl",
             lname: "Carlson", birthdate: "11/14/1971", sex: "male")
-u3 = User.create(email: "mrburns@yahoo.com", password: "charlesburns",
+
+file = File.open('app/assets/images/carl.png')
+f7.profile_pic = file
+f7.save!
+
+u3 = User.new(email: "mrburns@yahoo.com", password: "charlesburns",
             fname: "Charles",
             lname: "Burns", birthdate: "02/28/1899", sex: "male")
 
+file = File.open('app/assets/images/mrburns.png')
+u3.profile_pic = file
+u3.save!
+
 
 Profile.destroy_all
-Profile.create(user_id: User.first.id, category: "placesYouveLived",
+Profile.create(user_id: user.id, category: "placesYouveLived",
                sub_category: "currentCityAndHometown",
                value: "Lives In: Springfeild")
-Profile.create(user_id: User.first.id, category: "familyAndRelationships",
+Profile.create(user_id: user.id, category: "familyAndRelationships",
                sub_category: "familyMembers",
                value: "Son: Bart Simpson")
-Profile.create(user_id: User.first.id, category: "familyAndRelationships",
+Profile.create(user_id: user.id, category: "familyAndRelationships",
                sub_category: "familyMembers",
                value: "Daughter: Lisa Simpson")
-Profile.create(user_id: User.first.id, category: "familyAndRelationships",
+Profile.create(user_id: user.id, category: "familyAndRelationships",
                sub_category: "familyMembers",
                value: "Wife: Marge Simpson")
-Profile.create(user_id: User.first.id, category: "familyAndRelationships",
+Profile.create(user_id: user.id, category: "familyAndRelationships",
                sub_category: "relationship",
-               value: "Wife: Marge Simpson")
-Profile.create(user_id: User.first.id, category: "workAndEducation",
+               value: "Relationship: Marge Simpson")
+Profile.create(user_id: user.id, category: "workAndEducation",
                sub_category: "work",
                value: "Works At: Springfeild Nuclear Power Plant")
 
