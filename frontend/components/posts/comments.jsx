c2fa40d96b395e33e3f66e28ce4041bb8b2d3886
Tommy Ren <tommyr418@gmail.com>
2018-01-18 14:41:03 -0500

    position comment input and button

--- frontend/components/posts/comments.jsx
+++ frontend/components/posts/comments.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Comments = ({ postId, users, posts, comments }) => {
   const postComments = posts[postId].comments.map((commentId) => {
     const comment = comments[commentId];
     const authorId = comment.author_id;
+    const author = users[authorId];
 
     return (
       <div className="comment-item"
         key={ commentId }>
         <img id="comment-item-pic" src={ users[authorId].image_url }/>
         <p>
+          <Link to={ `/users/${author.id}` }>
+            { author.fname } { author.lname }
+          </Link>
           { comment.body }
         </p>
       </div>
