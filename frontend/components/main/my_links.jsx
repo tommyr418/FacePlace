20a64bf20ef16240428c949872b527c710fad7aa
Tommy Ren <tommyr418@gmail.com>
2018-02-20 13:04:29 -0500

    add links to news feed page

--- frontend/components/main/my_links.jsx
+++ frontend/components/main/my_links.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 const MyLinks = () => (
   <div id="my-links">
     <span>Tommy Ren</span>
+      <a href="https://github.com/tommyr418"
+        target="_blank">
+        <i className="fa fa-github" aria-hidden="true"></i>
+        <span className="label">GitHub</span>
+      </a>
 
+      <a href="https://www.linkedin.com/in/mengling-tommy-ren-967647152/"
+        target="_blank">
+        <i className="fa fa-linkedin" aria-hidden="true"></i>
+        <span className="label">LinkedIn</span>
+      </a>
+
+      <a href="https://angel.co/mengling-tommy-ren?public_profile=1"
+        target="_blank">
+        <i className="fa fa-angellist" aria-hidden="true"></i>
+        <span className="label">AngelList</span>
+      </a>
   </div>
 );
 
