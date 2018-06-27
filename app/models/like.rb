a6412057a7c58ca81f5444d417316569dde21e05
Tommy Ren <tommyr418@gmail.com>
2018-06-27 18:00:01 -0400

    add associations to likes

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer          not null
+#  likable_id   :integer          not null
+#  likable_type :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Like < ApplicationRecord
+  validates :user_id, :likable_id, presence: true
+  validates :user_id, uniqueness: { scope: [:likable_id, :likable_type] }
+
+  belongs_to :likable, polymorphic: true
+  belongs_to :user
 end
--- test/models/like_test.rb
+++ test/models/like_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id           :integer          not null, primary key
+#  user_id      :integer          not null
+#  likable_id   :integer          not null
+#  likable_type :string           not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 require 'test_helper'
 
 class LikeTest < ActiveSupport::TestCase
