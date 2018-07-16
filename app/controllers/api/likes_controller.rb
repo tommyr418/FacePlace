c5379ae446be34dea6b7d3285a1aaa80ba894cb4
Tommy Ren <tommyr418@gmail.com>
2018-07-15 23:24:14 -0400

    add params, create, destroy, function

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,2 +1,11 @@
 class Api::LikesController < ApplicationController
+  def create
+    @like = Like.new(like_params)
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:user_id, :likable_id, :likable_type)
+  end
 end
