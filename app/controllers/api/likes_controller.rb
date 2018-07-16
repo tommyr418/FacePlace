67788bbcaf60682761c2162b050c7f0d0b1e09d1
Tommy Ren <tommyr418@gmail.com>
2018-07-15 23:24:50 -0400

    add params, create, destroy, function

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,6 +1,22 @@
 class Api::LikesController < ApplicationController
   def create
     @like = Like.new(like_params)
+
+    if @like.save
+      render :show
+    else
+      render json: @like.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    if @like
+      @like.destroy
+      render json: {}
+    else
+      render json: ["Like does not exist"], status: 404
+    end
   end
 
   private
