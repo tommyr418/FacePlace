a514dbb3a7b210e9c6a5749724a88efecb1666db
Tommy Ren <tommyr418@gmail.com>
2018-08-06 23:15:33 -0400

    rename variables in seed file to make more readable

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,183 +9,183 @@
 require 'faker'
 
 User.destroy_all
-user1 = User.new(email: "homer@yahoo.com", password: "homersimpson",
+homer = User.new(email: "homer@yahoo.com", password: "homersimpson",
                    fname: "Homer", lname: "Simpson", birthdate: "04/17/1970",
                    sex: "male")
 
 file = File.open('app/assets/images/homer.jpg')
-user1.profile_pic = file
-user1.save!
+homer.profile_pic = file
+homer.save!
 
-user2 = User.new(email: "jon@yahoo.com", password: "jontargaryen",
+jon = User.new(email: "jon@yahoo.com", password: "jontargaryen",
                    fname: "Jon", lname: "Snow", birthdate: "04/18/1980",
                    sex: "male")
 
 file = File.open('app/assets/images/jon.jpeg')
-user2.profile_pic = file
-user2.save!
+jon.profile_pic = file
+jon.save!
 
-f1 = User.new(email: "marge@yahoo.com", password: "margesimpson",
+marge = User.new(email: "marge@yahoo.com", password: "margesimpson",
                  fname: "Marge", lname: "Simpson", birthdate: "10/10/1973",
                  sex: "female")
 
 file = File.open('app/assets/images/marge.png')
-f1.profile_pic = file
-f1.save!
+marge.profile_pic = file
+marge.save!
 
-f2 = User.new(email: "bart@yahoo.com", password: "bartsimpson", fname: "Bart",
+bart = User.new(email: "bart@yahoo.com", password: "bartsimpson", fname: "Bart",
             lname: "Simpson", birthdate: "12/30/1992", sex: "male")
 
 file = File.open('app/assets/images/bart.png')
-f2.profile_pic = file
-f2.save!
+bart.profile_pic = file
+bart.save!
 
-f3 =  User.new(email: "lisa@yahoo.com", password: "lisasimpson", fname: "Lisa",
+lisa =  User.new(email: "lisa@yahoo.com", password: "lisasimpson", fname: "Lisa",
             lname: "Simpson", birthdate: "01/18/1994", sex: "female")
 
 file = File.open('app/assets/images/lisa.png')
-f3.profile_pic = file
-f3.save!
+lisa.profile_pic = file
+lisa.save!
 
-u1 = User.new(email: "ned@yahoo.com", password: "nedflanders", fname: "Ned",
+ned = User.new(email: "ned@yahoo.com", password: "nedflanders", fname: "Ned",
             lname: "Flanders", birthdate: "12/25/1945", sex: "male")
 
 file = File.open('app/assets/images/ned.png')
-u1.profile_pic = file
-u1.save!
+ned.profile_pic = file
+ned.save!
 
-u2 = User.new(email: "maude@yahoo.com", password: "maudeflanders", fname: "Maude",
+maude = User.new(email: "maude@yahoo.com", password: "maudeflanders", fname: "Maude",
             lname: "Flanders", birthdate: "12/02/1952", sex: "female")
 
 file = File.open('app/assets/images/maude.png')
-u2.profile_pic = file
-u2.save!
+maude.profile_pic = file
+maude.save!
 
-f4 = User.new(email: "moe@yahoo.com", password: "moesizlack", fname: "Moe",
+moe = User.new(email: "moe@yahoo.com", password: "moesizlack", fname: "Moe",
             lname: "Sizlack", birthdate: "07/12/1959", sex: "male")
 
 file = File.open('app/assets/images/moe.jpg')
-f4.profile_pic = file
-f4.save!
+moe.profile_pic = file
+moe.save!
 
-f5 = User.new(email: "barney@yahoo.com", password: "barneygumble",
+barney = User.new(email: "barney@yahoo.com", password: "barneygumble",
             fname: "Barney",
             lname: "Gumble", birthdate: "05/14/1965", sex: "male")
 
 file = File.open('app/assets/images/barney.png')
-f5.profile_pic = file
-f5.save!
+barney.profile_pic = file
+barney.save!
 
-f6 = User.new(email: "lenny@yahoo.com", password: "lennyleonard", fname: "Lenny",
+lenny = User.new(email: "lenny@yahoo.com", password: "lennyleonard", fname: "Lenny",
             lname: "Leonard", birthdate: "11/27/1970", sex: "male")
 
 file = File.open('app/assets/images/lenny.jpg')
-f6.profile_pic = file
-f6.save!
+lenny.profile_pic = file
+lenny.save!
 
-f7 = User.new(email: "carl@yahoo.com", password: "carlcarlsom", fname: "Carl",
+carl = User.new(email: "carl@yahoo.com", password: "carlcarlsom", fname: "Carl",
             lname: "Carlson", birthdate: "11/14/1971", sex: "male")
 
 file = File.open('app/assets/images/carl.png')
-f7.profile_pic = file
-f7.save!
+carl.profile_pic = file
+carl.save!
 
-u3 = User.new(email: "mrburns@yahoo.com", password: "charlesburns",
+burns = User.new(email: "mrburns@yahoo.com", password: "charlesburns",
             fname: "Charles",
             lname: "Burns", birthdate: "02/28/1899", sex: "male")
 
 file = File.open('app/assets/images/mrburns.png')
-u3.profile_pic = file
-u3.save!
+burns.profile_pic = file
+burns.save!
 
-u4 = User.new(email: "daenerys@yahoo.com", password: "dtargaryen",
+daenerys = User.new(email: "daenerys@yahoo.com", password: "dtargaryen",
             fname: "Daenerys",
             lname: "Targaryen", birthdate: "08/28/1989", sex: "female")
 
 file = File.open('app/assets/images/daenerys.jpg')
-u4.profile_pic = file
-u4.save!
+daenerys.profile_pic = file
+daenerys.save!
 
-u5 = User.new(email: "arya@yahoo.com", password: "aryastark",
+arya = User.new(email: "arya@yahoo.com", password: "aryastark",
             fname: "Arya",
             lname: "Stark", birthdate: "01/23/1996", sex: "female")
 
 file = File.open('app/assets/images/arya.jpg')
-u5.profile_pic = file
-u5.save!
+arya.profile_pic = file
+arya.save!
 
-u6 = User.new(email: "bran@yahoo.com", password: "branstark",
+bran = User.new(email: "bran@yahoo.com", password: "branstark",
             fname: "Bran",
             lname: "Stark", birthdate: "07/10/1994", sex: "male")
 
 file = File.open('app/assets/images/bran.jpg')
-u6.profile_pic = file
-u6.save!
+bran.profile_pic = file
+bran.save!
 
-u7 = User.new(email: "sansa@yahoo.com", password: "sansastark",
+sansa = User.new(email: "sansa@yahoo.com", password: "sansastark",
             fname: "Sansa",
             lname: "Stark", birthdate: "4/10/1990", sex: "female")
 
 file = File.open('app/assets/images/sansa.jpg')
-u7.profile_pic = file
-u7.save!
+sansa.profile_pic = file
+sansa.save!
 
 
 Profile.destroy_all
-Profile.create(user_id: user1.id, category: "placesYouveLived",
+Profile.create(user_id: homer.id, category: "placesYouveLived",
                sub_category: "currentCityAndHometown",
                value: "Lives In: Springfeild")
-Profile.create(user_id: user1.id, category: "familyAndRelationships",
+Profile.create(user_id: homer.id, category: "familyAndRelationships",
                sub_category: "familyMembers",
                value: "Son: Bart Simpson")
-Profile.create(user_id: user1.id, category: "familyAndRelationships",
+Profile.create(user_id: homer.id, category: "familyAndRelationships",
                sub_category: "familyMembers",
                value: "Daughter: Lisa Simpson")
-Profile.create(user_id: user1.id, category: "familyAndRelationships",
+Profile.create(user_id: homer.id, category: "familyAndRelationships",
                sub_category: "familyMembers",
                value: "Wife: Marge Simpson")
-Profile.create(user_id: user1.id, category: "familyAndRelationships",
+Profile.create(user_id: homer.id, category: "familyAndRelationships",
                sub_category: "relationship",
                value: "Relationship: Marge Simpson")
-Profile.create(user_id: user1.id, category: "workAndEducation",
+Profile.create(user_id: homer.id, category: "workAndEducation",
                sub_category: "work",
                value: "Works At: Springfeild Nuclear Power Plant")
 
 FriendRequest.destroy_all
-FriendRequest.create(requester_id: user1.id,
-                     recipient_id: f1.id, status: "completed")
-FriendRequest.create(requester_id: user1.id,
-                     recipient_id: f2.id, status: "completed")
-FriendRequest.create(requester_id: f3.id,
-                     recipient_id: user1.id, status: "completed")
-FriendRequest.create(requester_id: user1.id,
-                     recipient_id: f4.id, status: "completed")
-FriendRequest.create(requester_id: f5.id,
-                     recipient_id: user1.id, status: "completed")
-FriendRequest.create(requester_id: f6.id,
-                     recipient_id: user1.id, status: "completed")
-FriendRequest.create(requester_id: user1.id,
-                     recipient_id: f7.id, status: "completed")
-FriendRequest.create(requester_id: f3.id,
-                     recipient_id: f2.id, status: "completed")
-FriendRequest.create(requester_id: u1.id,
-                     recipient_id: user1.id)
-FriendRequest.create(requester_id: u3.id,
-                     recipient_id: user1.id)
-FriendRequest.create(requester_id: f6.id,
-                     recipient_id: u3.id, status: "completed")
+FriendRequest.create(requester_id: homer.id,
+                     recipient_id: marge.id, status: "completed")
+FriendRequest.create(requester_id: homer.id,
+                     recipient_id: bart.id, status: "completed")
+FriendRequest.create(requester_id: lisa.id,
+                     recipient_id: homer.id, status: "completed")
+FriendRequest.create(requester_id: homer.id,
+                     recipient_id: moe.id, status: "completed")
+FriendRequest.create(requester_id: barney.id,
+                     recipient_id: homer.id, status: "completed")
+FriendRequest.create(requester_id: lenny.id,
+                     recipient_id: homer.id, status: "completed")
+FriendRequest.create(requester_id: homer.id,
+                     recipient_id: carl.id, status: "completed")
+FriendRequest.create(requester_id: lisa.id,
+                     recipient_id: bart.id, status: "completed")
+FriendRequest.create(requester_id: ned.id,
+                     recipient_id: homer.id)
+FriendRequest.create(requester_id: burns.id,
+                     recipient_id: homer.id)
+FriendRequest.create(requester_id: lenny.id,
+                     recipient_id: burns.id, status: "completed")
 
 Friend.destroy_all
-Friend.create(friender_id: user1.id, friendee_id: f1.id)
-Friend.create(friender_id: user1.id, friendee_id: f2.id)
-Friend.create(friender_id: f3.id, friendee_id: user1.id)
-Friend.create(friender_id: user1.id, friendee_id: f4.id)
-Friend.create(friender_id: f5.id, friendee_id: user1.id)
-Friend.create(friender_id: f6.id, friendee_id: user1.id)
-Friend.create(friender_id: user1.id, friendee_id: f7.id)
+Friend.create(friender_id: homer.id, friendee_id: marge.id)
+Friend.create(friender_id: homer.id, friendee_id: bart.id)
+Friend.create(friender_id: lisa.id, friendee_id: homer.id)
+Friend.create(friender_id: homer.id, friendee_id: moe.id)
+Friend.create(friender_id: barney.id, friendee_id: homer.id)
+Friend.create(friender_id: lenny.id, friendee_id: homer.id)
+Friend.create(friender_id: homer.id, friendee_id: carl.id)
 
 User.all.each do |person|
-  next if user1.id == person.id
+  next if homer.id == person.id
   2.times do
     random = User.all.sample.id
     while(person.friends.include?(random) || person.id == random)
