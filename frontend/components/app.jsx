e531e4d7a39134b6fb3beafecbc9dbd6138a0190
Tommy Ren <tommyr418@gmail.com>
2019-03-05 15:51:30 -0500

    relocate footer css to own file

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,7 +5,7 @@ import { ProtectedRoute } from '../util/route_util';
 import Header from './header/header_container';
 import MainContainer from './main/main_container';
 import MainShowContainer from './main-show/main_show_container';
-import MainFooterContainer from './footer/main_footer_container';
+import MainFooterContainer from './footer/footer_container';
 import SearchContainer from './search/search_container';
 
 const App = () => (
--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -51,7 +51,7 @@ class Header extends React.Component {
   render () {
     if(this.props.currentUser){
       return (
-        <header id="header-main-logged-in">
+        <div id="header-main-logged-in">
           <div id="header-left-logged-in">
             <a href="/"><div id="logo">fP</div></a>
             <form onSubmit={ this.handleSubmit }>
@@ -73,16 +73,16 @@ class Header extends React.Component {
             fetchRequesters={ this.props.fetchRequesters }
             addFriend={ this.props.addFriend }
             updateRequest={ this.props.updateRequest }/>
-        </header>
+        </div>
       );
     } else {
       return (
-        <header id="header-main">
+        <div id="header-main">
           <h1>facePlace</h1>
 
           <LoginForm login={ this.props.login }
             errors={ this.props.errors }/>
-        </header>
+        </div>
       );
     }
   }
