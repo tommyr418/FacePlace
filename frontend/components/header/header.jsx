03a853e6db3f17024334827ab241e74124efd220
Tommy Ren <tommyr418@gmail.com>
2019-05-09 11:16:10 -0400

    merge point

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import './header.css';
 import LoginForm from './login_form/login_form';
