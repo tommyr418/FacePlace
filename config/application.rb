35a9f350c5dabc4cead8fac147f9781f224d3183
Tommy Ren <tommyr418@gmail.com>
2019-11-20 16:57:37 -0500

    update config files for 6.0.1

--- config/application.rb
+++ config/application.rb
@@ -12,17 +12,8 @@ module FacebookLite
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-        :bucket => ENV["s3_bucket"],
-        :access_key_id => ENV["s3_access_key_id"],
-        :secret_access_key => ENV["s3_secret_access_key"],
-        :s3_region => ENV["s3_region"],
-        :s3_host_name => "s3.#{ENV["s3_region"]}.amazonaws.com"
-      }
-    }
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
